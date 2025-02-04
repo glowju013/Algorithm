9bc3045ffedf1761a4b74f4826d102eee8f45e83
yunju Lee <76641389+glowju013@users.noreply.github.com>
2025-02-04 21:08:19 +0900

    Update 세로읽기.java
    chore: max값 삭제

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = -999;
         char[][] arr = new char[5][15];
         for (int i=0; i<5; i++) {
             String str = br.readLine();
